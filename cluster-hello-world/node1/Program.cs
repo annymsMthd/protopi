e824729226234f4e087bca9bcfb5fb4d27e0a622
annymsmthd <benjamin@syncromatics.com>
2017-06-10 16:07:17 -0700

    add cluster helloworld

---
+++ cluster-hello-world/node1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.Loader;
+using System.Threading;
+using System.Threading.Tasks;
+using Messages;
+using Proto.Cluster;
+using Proto.Cluster.Consul;
+using Proto.Remote;
+using ProtosReflection = Messages.ProtosReflection;
+
+namespace node1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(10000);
+            var endsignal = new ManualResetEventSlim();
+
+            AssemblyLoadContext.Default.Unloading += ctx =>
+            {
+                System.Console.WriteLine("Unloading fired");
+                endsignal.Set();
+            };
+
+            Serialization.RegisterFileDescriptor(ProtosReflection.Descriptor);
+
+            Remote.Start("node1", 12001);
+
+            Cluster.Start("MyCluster", new ConsulProvider(new ConsulProviderOptions(), config =>
+            {
+                config.Address = new Uri("http://consul1:8500");
+            }));
+
+            Console.WriteLine("node1 started");
+
+            Task.Run(async () =>
+            {
+                var pid = await Cluster.GetAsync("TheName", "HelloKind");
+
+                var res = await pid.RequestAsync<HelloResponse>(new HelloRequest());
+
+                Console.WriteLine(res.Message);
+            });
+
+            endsignal.Wait();
+        }
+    }
+}
---
+++ cluster-hello-world/node2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.Loader;
+using System.Threading;
+using Messages;
+using Proto;
+using Proto.Cluster;
+using Proto.Cluster.Consul;
+using Proto.Remote;
+using ProtosReflection = Messages.ProtosReflection;
+
+namespace node1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var endsignal = new ManualResetEventSlim();
+
+            AssemblyLoadContext.Default.Unloading += ctx =>
+            {
+                System.Console.WriteLine("Unloading fired");
+                endsignal.Set();
+            };
+
+            Serialization.RegisterFileDescriptor(ProtosReflection.Descriptor);
+            var props = Actor.FromFunc(ctx =>
+            {
+                switch (ctx.Message)
+                {
+                    case HelloRequest _:
+                        ctx.Respond(new HelloResponse
+                        {
+                            Message = "Hello from node 2"
+                        });
+                        break;
+                }
+                return Actor.Done;
+            });
+
+            Remote.RegisterKnownKind("HelloKind", props);
+            Remote.Start("node2", 12000);
+            Cluster.Start("MyCluster", new ConsulProvider(new ConsulProviderOptions(), config =>
+            {
+                config.Address = new Uri("http://consul1:8500");
+            }));
+
+            Console.WriteLine("node2 started");
+
+            endsignal.Wait();
+        }
+    }
+}

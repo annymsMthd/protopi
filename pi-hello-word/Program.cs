11bb1765b7e8ab4459f5c3ee1d1c946850ea545b
annymsmthd <benjamin@syncromatics.com>
2017-06-10 23:31:42 -0700

    add the pi hello world

---
+++ pi-hello-word/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.Loader;
+using System.Threading;
+using System.Threading.Tasks;
+using Proto;
+
+namespace pi_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var endsignal = new ManualResetEventSlim();
+
+            AssemblyLoadContext.Default.Unloading += ctx =>
+            {
+                System.Console.WriteLine("Unloading fired");
+                endsignal.Set();
+            };
+
+            var props = Actor.FromProducer(() => new HelloActor());
+            var pid = Actor.Spawn(props);
+            pid.Tell(new Hello
+            {
+                Who = "ProtoActor"
+            });
+
+            Console.WriteLine("actor system started");
+            endsignal.Wait();
+        }
+
+        internal class Hello
+        {
+            public string Who;
+        }
+
+        internal class HelloActor : IActor
+        {
+            public Task ReceiveAsync(IContext context)
+            {
+                var msg = context.Message;
+                if (msg is Hello r)
+                {
+                    Console.WriteLine($"Hello {r.Who}");
+                }
+                return Actor.Done;
+            }
+        }
+    }
+}
